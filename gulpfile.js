7996d409b090c9aab29a78faf69ef73d7d26ccf1
morinpic <morinpic0518@gmail.com>
2014-09-19 19:46:08 +0900

    browser-sync追加

--- gulpfile.js
+++ gulpfile.js
@@ -9,20 +9,32 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var minify = require('gulp-minify-css');
 var plumber = require('gulp-plumber');
+var browser = require("browser-sync");
 
+// server
+gulp.task("server", function() {
+    browser({
+        server: {
+            baseDir: path.tmp
+        }
+    });
+});
+
+// sass
 gulp.task('sass', function(){
   gulp.src(path.src + '/scss/**/*.scss')
     .pipe(plumber())
     .pipe(sass())
     .pipe(minify())
-    .pipe(gulp.dest(path.tmp + '/css/'));
+    .pipe(gulp.dest(path.tmp + '/css/'))
+    .pipe(browser.reload({stream:true}));
 });
 
+// watch
 gulp.task('watch', function(){
   gulp.watch([path.src +'/scss/**/*.scss'], ['sass']);
 });
 
-gulp.task('default', [
-  'sass',
-  'watch'
-]);
+gulp.task('default', ['server'], function() {
+  gulp.watch([path.src +'/scss/**/*.scss'], ['sass']);
+});
