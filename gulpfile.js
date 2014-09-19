caf449d98fadb607df8db199ec03f58d57e7260b
morinpic <morinpic0518@gmail.com>
2014-09-19 17:40:45 +0900

    scss環境初期設定

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,17 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 
-gulp.task('sass',function(){
-  gulp.src('sass/**/*.scss')
+gulp.task('sass', function(){
+  gulp.src('assets/scss/**/*.scss')
     .pipe(sass())
-    .pipe(gulp.dest('css/'));
+    .pipe(gulp.dest('assets/css/'));
 });
 
-gulp.task('default', ['sass']);
+gulp.task('watch', function(){
+  gulp.watch(['assets/scss/**/*.scss'], ['sass']);
+});
+
+gulp.task('default', [
+  'sass',
+  'watch'
+]);
