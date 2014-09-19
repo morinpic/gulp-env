1f39639d46055295f1e9c4d5155aa24389ba64aa
morinpic <morinpic0518@gmail.com>
2014-09-19 18:49:31 +0900

    基本参照先、出力先フォルダ設定 assets ソースフォルダ .tmp   開発中フォルダ build  ビルドフォルダ

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,23 @@
+// basic path setting
+var path = {
+  src : 'assets',
+  tmp : '.tmp',
+  dist : 'build'
+};
+
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var minify = require('gulp-minify-css');
 
 gulp.task('sass', function(){
-  gulp.src('assets/scss/**/*.scss')
+  gulp.src(path.src + '/scss/**/*.scss')
     .pipe(sass())
     .pipe(minify())
-    .pipe(gulp.dest('assets/css/'));
+    .pipe(gulp.dest(path.tmp + '/css/'));
 });
 
 gulp.task('watch', function(){
-  gulp.watch(['assets/scss/**/*.scss'], ['sass']);
+  gulp.watch([path.src +'/scss/**/*.scss'], ['sass']);
 });
 
 gulp.task('default', [
